925112d82a361b1cce242f0a61eec64398f9eab1
Aneta590 <anetalisiecka.al@gmail.com>
2021-07-08 15:18:32 +0100

    form

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {TextInput,Alert, Button, Form, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const[login, setLogin]= useState('')
+  const[password,setPassword]= useState('')
+
+  const Validation= () =>{
+  console.log (login.length > 6 && password.length > 6)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+        <form>
+  <label> 
+    Login:
+    <TextInput type="text" value={login} onChangeText={setLogin}/>
+  </label>
+  
+</form>
+
+<form>
+  <label>
+    Password:
+    <TextInput type="text" value={password} onChangeText={setPassword} />
+  </label>
+  
+  <Button
+          title="Validate"
+          onPress={ Validation }
+        />
+
+</form>
     </View>
   );
 }
